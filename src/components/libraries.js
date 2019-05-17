a29175657a32ad4fb1c1d08a544debff3f2eb1b2
Ernesto Cruz Olivera <ernesto@ecruzolivera.tech>
2019-05-17 16:05:13 -0600

    material ui added

---
+++ src/components/libraries.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Helmet from "react-helmet"
+
+export default () => (
+  <Helmet>
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
+    />
+  </Helmet>
+)
+
