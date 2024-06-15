d1b5fca67d5d3cd4b5158e82425c4799f4aa3a1d
Ernesto Cruz <ecruzolivera@gmail.com>
2024-06-15 01:05:33 -0600

    enabling rss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,19 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config"
+import mdx from "@astrojs/mdx"
+import sitemap from "@astrojs/sitemap"
 
 // import cloudflare from "@astrojs/cloudflare"
 
-import icon from "astro-icon";
+import icon from "astro-icon"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://ecruzolivera-website.pages.dev",
   integrations: [mdx(), sitemap(), icon()],
-  output: "static"
+  output: "static",
   // adapter: cloudflare({
   //   platformProxy: {
   //     enabled: true,
   //   },
   // }),
-});
+})
